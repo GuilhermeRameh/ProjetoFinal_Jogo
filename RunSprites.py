ec16fc9648ed6b50801c72da4653fc803fc9882e
Guilherme Rameh <guilhermedr@al.insper.edu.br>
2020-06-14 10:12:18 -0300

    Este é o arquivo .zip contendo os ajustes finais para a entrega do jogo. Tudo que voce precisa saber para jogar esse jogo está aí! Boa sorte, e tente não falecer!

--- RunSprites.py
+++ RunSprites.py
@@ -5,4 +5,15 @@ Setup
 pygame.init()
 pygame.mixer.init()
 
-fundo_1 = pygame.image.load("Sprites\Fundo1.png").convert()
+fundo_1 = pygame.image.load("Sprites/Fundo1.png").convert()
+kleber = pygame.image.load("Sprites/Kléber64x64.png").convert_alpha()
+kleber = pygame.transform.scale2x(kleber)
+jorge = pygame.image.load("Sprites/Jorge64x64.png").convert_alpha()
+jorge = pygame.transform.scale2x(jorge)
+heart = {
+    100: pygame.image.load("Sprites/HeartFull.png").convert_alpha(),
+    75: pygame.image.load("Sprites/Heart75.png").convert_alpha(),
+    50: pygame.image.load("Sprites/Heart50.png").convert_alpha(),
+    25: pygame.image.load("Sprites/Heart25.png").convert_alpha(),
+    0: pygame.image.load("Sprites/Heart0.png").convert_alpha()
+}
--- TelaInicial.py
+++ TelaInicial.py
@@ -1,9 +1,9 @@
 import Setup
+import Stats_Base
 import pygame
 
+def Inicio():  
 
-def Inicio():
-        
     texto=Setup.inicio.render('Bem vindo a Kléber e a Dungeon',1,(255,255,255))
     infinitaa=Setup.infinita.render('INFINITA!',1,(255,255,255))
     botão_iniciar=Setup.iniciar.render('Clique em qualquer lugar da tela para iniciar o jogo',1,(255,255,255))
